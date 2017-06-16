c238a2724a6985c20d63eb662348c92256abbb30
Kenner <kenner3.1@gmail.com>
2017-06-16 23:48:35 +0200

    Actualización de neurona, soporte para conexiones.

--- neurona.c
+++ neurona.c
@@ -1,13 +1,39 @@
 typedef struct neurona{
 
 	float valor;
-	uint32 conexiones;
-	uint32 funciones;
+
+	uint32* conexiones;
+	uint32* funciones;
+
+	uint32 n_conexiones = 0;
+
 
 }Neurona;
+
+
+void neurona_conectar( Neurona* n, uint32 neurona_capa_siguiente, uint32 funcion ){
+
+	n->n_conexiones += 1;
+
+	if( n->n_conexiones == 1 ){
+		n->conexiones = malloc( sizeof(uint32) * n->n_conexiones );
+		n->funciones  = malloc( sizeof(uint32) * n->n_conexiones );
+	}
+	else{
+		n->conexiones = realloc( n->conexiones, sizeof(uint32) * n->n_conexiones );
+		n->funciones  = realloc( n->funciones, sizeof(uint32) * n->n_conexiones );
+	}
+}
+
 
 void neurona_printear( Neurona* n ){
 
 	printf("Valor: %f\n", n->valor);
+
+	printf("Numero de conexiones = %d\n", n->n_conexiones );
+
+	for(int i = 0; i < n->n_conexiones; ++i){
+		printf("Conexion %d: conectado con neurona %d con funcion %d\n", i, n->conexiones[i], n->funciones[i] );
+	}
 
 }
