0663dd176f8601a61ce1f95e92f808a308cfbd20
Kenner Corps <kenner3.1@gmail.com>
2017-06-19 13:32:57 +0200

    Añadidas funciones básicas de propagación.

--- propagador.c
+++ propagador.c
@@ -1,17 +1,64 @@
-void propagador_neurona( Neurona* n ){
 
+void propagador_funcion( float valor, uint32 funcion, Neurona* destino ){
+
+	switch( funcion ){
+
+		case 0:	// Pasar el valor
+			
+			destino->valor = valor;
+			break;
+
+		case 1:
+
+			break;
+		default:
+			puts("Funcion no establecida");
+	}
+
+
+}
+
+void propagador_neurona( Neurona* n, Capa* c_siguiente ){
+
+	uint32 n_conexiones = n->n_conexiones;
+
+	for (int i = 0; i < n_conexiones; ++i){
+		
+		propagador_funcion( n->valor, n->funciones[ i ], &c_siguiente->neuronas[ n->conexiones[ i ] ] );
+
+	}
+
+}
+
+void propagador_capa( Capa* c_actual, Capa* c_siguiente ){
+
+	uint32 n_neuronas = c_actual->n_neuronas;
+
+	for (int i = 0; i < n_neuronas; ++i){
+		
+		propagador_neurona( &c_actual->neuronas[i], c_siguiente );
+
+	}
 }
 
 void propagador_ejecutar( Cerebro* c ){
 
-	// Ejecutar las neuronas de la entrada:
-	for (int i = 0; i < c->n_entradas; ++i){
+	Capa* capa_actual;
+	Capa* capa_siguiente;
+
+	capa_siguiente = c->entrada;
+
+	for (int i = 0; i < c->n_capas; ++i){
 		
+		capa_actual = capa_siguiente;
+		capa_siguiente = c->capas[ i ];
+
+		propagador_capa( capa_actual, capa_siguiente );
 
 	}
-	// Ejecutar las neuronas de las capas intermedias:
-
-	// Salida:
 	
+	capa_actual = capa_siguiente;
+	capa_siguiente = c->salida;
 
+	propagador_capa( capa_actual, capa_siguiente );
 }
