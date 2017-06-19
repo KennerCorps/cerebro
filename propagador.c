a612728e036f12584f81f7626ad44f1a5fe946ff
Kenner Corps <kenner3.1@gmail.com>
2017-06-19 12:32:38 +0200

    Propagación de datos entre neuronas.

--- propagador.c
+++ propagador.c
@@ -1,5 +1,17 @@
+void propagador_neurona( Neurona* n ){
+
+}
+
 void propagador_ejecutar( Cerebro* c ){
 
-	;
+	// Ejecutar las neuronas de la entrada:
+	for (int i = 0; i < c->n_entradas; ++i){
+		
+
+	}
+	// Ejecutar las neuronas de las capas intermedias:
+
+	// Salida:
+	
 
 }
