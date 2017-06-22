d5024ff842dc196e8cda43250f9343fd33c6a1f7
Kenner Corps <kenner3.1@gmail.com>
2017-06-22 14:36:52 +0200

    Cerebro identificador de patrones.

--- mi_cerebro.c
+++ mi_cerebro.c
@@ -19,6 +19,47 @@ Cerebro* crear_mi_cerebro(){
 
 	capa_mostrar( capa_oculta_0 );
 
+
+	neurona_conectar( &entrada->neuronas[ 0 ], 0 );
+	neurona_conectar( &entrada->neuronas[ 0 ], 2 );
+	neurona_conectar( &entrada->neuronas[ 1 ], 1 );
+	neurona_conectar( &entrada->neuronas[ 1 ], 3 );
+	neurona_conectar( &entrada->neuronas[ 2 ], 1 );
+	neurona_conectar( &entrada->neuronas[ 2 ], 3 );
+	neurona_conectar( &entrada->neuronas[ 3 ], 0 );
+	neurona_conectar( &entrada->neuronas[ 3 ], 2 );
+
+
+	neurona_conectar( &capa_oculta_0->neuronas[ 0 ], 0 );
+	neurona_conectar( &capa_oculta_0->neuronas[ 0 ], 1 );
+	neurona_conectar( &capa_oculta_0->neuronas[ 1 ], 0 );
+	neurona_conectar( &capa_oculta_0->neuronas[ 1 ], 1 );
+	neurona_conectar( &capa_oculta_0->neuronas[ 2 ], 2 );
+	neurona_conectar( &capa_oculta_0->neuronas[ 2 ], 3 );
+	neurona_conectar( &capa_oculta_0->neuronas[ 3 ], 2 );
+	neurona_conectar( &capa_oculta_0->neuronas[ 3 ], 3 );
+
+
+	neurona_conectar( &capa_oculta_1->neuronas[ 0 ], 0 );
+	neurona_conectar( &capa_oculta_1->neuronas[ 0 ], 1 );
+	neurona_conectar( &capa_oculta_1->neuronas[ 1 ], 2 );
+	neurona_conectar( &capa_oculta_1->neuronas[ 1 ], 3 );
+	neurona_conectar( &capa_oculta_1->neuronas[ 2 ], 4 );
+	neurona_conectar( &capa_oculta_1->neuronas[ 2 ], 5 );
+	neurona_conectar( &capa_oculta_1->neuronas[ 3 ], 6 );
+	neurona_conectar( &capa_oculta_1->neuronas[ 3 ], 7 );
+
+
+	neurona_conectar( &capa_oculta_2->neuronas[ 0 ], 0 );
+	neurona_conectar( &capa_oculta_2->neuronas[ 1 ], 0 );
+	neurona_conectar( &capa_oculta_2->neuronas[ 2 ], 1 );
+	neurona_conectar( &capa_oculta_2->neuronas[ 3 ], 1 );
+	neurona_conectar( &capa_oculta_2->neuronas[ 4 ], 2 );
+	neurona_conectar( &capa_oculta_2->neuronas[ 5 ], 2 );
+	neurona_conectar( &capa_oculta_2->neuronas[ 6 ], 3 );
+	neurona_conectar( &capa_oculta_2->neuronas[ 7 ], 3 );
+
+
 	cerebro_insertar_entrada( c, entrada );
 	cerebro_insertar_salida( c, salida );
 
